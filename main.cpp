2bf0473dd30f1eea2e617e1075854f8c32549b49
uzunb <batuuzun08@gmail.com>
2024-08-20 20:39:25 +0300

    feat(ui): initial web server

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <drogon/HttpAppFramework.h>
+
+int main() {
+    // Set HTTP listener address and port
+    drogon::app().addListener("127.0.0.1", 80);
+
+    // Load config file
+    drogon::app().loadConfigFile("../ui/config.json");
+
+    std::cout << "Web server is running" << std::endl;
+    std::cout << "Connect to http://127.0.0.1:80" << std::endl;
+
+    // Run HTTP framework,the method will block in the internal event loop
+    drogon::app().run();
+    return 0;
+}
